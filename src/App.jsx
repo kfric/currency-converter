d4f01b95c5f7c73fa780f6e596653785378baf8c
Karl Frick <karl.frick5@gmail.com>
2021-06-02 13:02:39 -0400

    Initail HTML and CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
 import React from 'react'
 
 export function App() {
-  return <div>Hello, World!</div>
+  return (
+    <div>
+      <header>
+        <h1>Currency Converter</h1>
+      </header>
+      <main>
+        <section>
+          $<input type="text" /> USD
+        </section>
+        <section>
+          <ul>
+            <li>country: $$ </li>
+            <li>country: $$</li>
+            <li>country: </li>
+            <li>country: </li>
+            <li>country: </li>
+          </ul>
+        </section>
+      </main>
+    </div>
+  )
 }
